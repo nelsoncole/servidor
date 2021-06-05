6f35fcf4e733a310820b68f695431c38442fe679
nelsoncole <nelsoncole72@gmail.com>
2021-06-05 17:43:54 +0100

    Add files via upload

---
+++ itoa.c
@@ -0,0 +1,37 @@
+#include <limits.h>
+
+char *itoa(int val, char *dst, int radix)
+{
+	char* valuestring = (char*) dst;
+        int value = val;
+                                                                                                             int min_flag;
+        char swap, *p;
+        min_flag = 0;
+
+	 if (0 > value)
+	{
+                *valuestring++ = '-';                                                                                value = -INT_MAX> value ? min_flag = INT_MAX : -value;
+        }
+
+        p = valuestring;
+        do
+        {
+                *p++ = (char)(value % 10) + '0';
+                value /= 10;
+        } while (value);
+
+        if (min_flag != 0)
+        {
+                ++*valuestring;
+        }
+        *p-- = '\0';
+
+        while (p > valuestring)
+        {
+                swap = *valuestring;
+                *valuestring++ = *p;
+                *p-- = swap;
+        }
+
+        return dst;
+}
